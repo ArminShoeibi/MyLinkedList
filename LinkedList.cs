0873cc08ba14d6b1722fa779b846ec3ec2e37da1
armin <a.shoeibi@alibaba.ir>
2021-10-31 23:54:49 +0330

    Add project files.

---
+++ LinkedList.cs
@@ -0,0 +1,62 @@
+﻿namespace MyLinkedList;
+
+public class LinkedList<T>
+{
+    private Node _first;
+    private Node _last;
+
+    private class Node
+    {
+        public T _value;
+        public Node _next;
+        public Node(T value)
+        {
+            _value = value;
+        }
+    }
+    public void AddLast(T item)
+    {
+        Node node = new(item);
+
+        if (IsHeadNull())
+            _first = _last = node;
+        else
+        {
+            _last._next = node;
+            _last = node;
+        }
+    }
+
+    public void AddFirst(T item)
+    {
+        Node node = new(item);
+        if (IsHeadNull())
+            _first = _last = node;
+        else
+        {
+            node._next = _first;
+            _first = node;
+        }
+    }
+
+    public int IndexOf(T item)
+    {
+        int index = 0;
+        var current = _first;
+        while (current != null)
+        {
+            if (EqualityComparer<T>.Default.Equals(current._value, item))
+                return index;
+            current = current._next;
+            index++;
+        }
+        return -1;
+    }
+
+    public bool Contains(T item)
+    {
+        return IndexOf(item) != 1;
+    }
+
+    private bool IsHeadNull() => _first is null;
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using MyLinkedList;
+
+MyLinkedList.LinkedList<int> linkedList = new();
+linkedList.AddLast(10);
+linkedList.AddLast(20);
+linkedList.AddLast(30);
+
+var indexOfTen = linkedList.IndexOf(10);
+var indexOfTwenty = linkedList.IndexOf(20);
+
+Console.WriteLine();
+
